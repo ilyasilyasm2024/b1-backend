a71dbd4f8225b24a3d068b5aff8b19a28f90f30e
Ilyas Qabbal <i.qabbal@navimarsystems.com>
2026-07-15 14:05:28 +0100

    fix notes bugs

--- src/modules/notes/notes.model.js
+++ src/modules/notes/notes.model.js
@@ -8,6 +8,8 @@ const noteSchema = new mongoose.Schema({
   color: { type: String, default: '#fde68a' },
   dir: { type: String, enum: ['ltr', 'rtl'], default: 'ltr' },
   collapsed: { type: Boolean, default: false },
+  pinned: { type: Boolean, default: false },
+  order: { type: Number, default: 0 },
   links: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }],
   x: { type: Number, default: 100 },
   y: { type: Number, default: 120 },
